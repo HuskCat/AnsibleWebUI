db646a3d582158e38c6a02e8527ff1c23682e965
Nanami1227 <34113239+nanami1227@users.noreply.github.com>
2023-05-11 13:06:40 +0800

    Add module

--- src/main.js
+++ src/main.js
@@ -10,11 +10,15 @@ import router from './router'
 import VueHighlightJS from 'vue3-highlightjs'
 import 'vue3-highlightjs/styles/solarized-dark.css'
 
+import Dropdown from 'vue-simple-search-dropdown';
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.use(VueHighlightJS)
+
+app.use(Dropdown)
 
 app.mount('#app')
