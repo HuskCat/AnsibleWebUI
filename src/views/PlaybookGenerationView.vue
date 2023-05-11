26001db8a90be23b0a2a405cb776e11de735c016
Nanami1227 <34113239+nanami1227@users.noreply.github.com>
2023-05-11 13:08:27 +0800

    Add Multi tasks and vars

--- src/views/PlaybookGenerationView.vue
+++ src/views/PlaybookGenerationView.vue
@@ -8,8 +8,8 @@ export default {
       targetHost: 'Hosts',
       taskName: 'Task Name',
       moduleName: 'Module Name',
-      save: '已儲存',
-      run: '正在執行劇本'
+      save: 'Playbook Saved',
+      run: 'Running Playbook'
     }
   },
   methods: {
@@ -19,7 +19,7 @@ export default {
     },
     runPlaybook() {
       // TODO: console run ansible-playbook
-      return alert(this.run + '"' + this.playbookName + '"')
+      return alert(this.run + ' "' + this.playbookName + '"')
     }
   },
   computed: {
@@ -34,12 +34,21 @@ export default {
 <template>
   <div class="playbook-generation">
     <input type="text" v-model="playbookName" />
+    <div class="playbook-generation-modify">
+      <div class="playbook-generation-options"><!-- TODO: Vars here--></div>
+      <button class="addButton" type="button" @click="addVars">Add variables</button>
+    </div>
     <input type="text" v-model="targetHost" />
+    <div class="playbook-generation-modify">
+      <div class="playbook-generation-options">
     <input type="text" v-model="taskName" />
     <input type="text" v-model="moduleName" /><!-- TODO:Set module list dropdown menu -->
     <!-- TODO:Add module options -->
-    <pre v-highlightjs="sourcecode">
-      <code class="javascript">
+    <!-- TODO: New tasks here--></div>
+    <button class="addButton" type="button" @click="addTask">New tasks</button></div>
+    <div class="playbook-generation-preview">
+      <pre v-highlightjs="sourcecode">
+      <code class="yaml">
 ---
 - name: {{ playbookName }}
   hosts: {{ targetHost }}
@@ -48,6 +57,7 @@ export default {
       {{ moduleName }}:
       </code>
     </pre>
+    </div>
     <div class="playbook-generation-buttons">
       <button type="button" @click="savePlaybook()">Save</button>
       <button type="button" @click="runPlaybook()">Run</button>
@@ -58,17 +68,33 @@ export default {
 <style>
 @media (min-width: 1024px) {
   .playbook-generation {
-    min-height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
   }
   .playbook-generation-buttons {
-    display: flex;
     flex-direction: row;
-    justify-content: center;
-    align-items: center;
+  }
+  .playbook-generation-modify {
+    flex-direction: row;
+  }
+  .playbook-generation-options {
+    display: flex;
+    flex-direction: column;
+  }
+  .playbook-generation-preview {
+    text-align: start;
+  }
+  button{
+    width:50%;
+  }
+  button.addButton {
+    width: 500px;
+  }
+  input{
+    width: 500px;
   }
 }
 </style>
